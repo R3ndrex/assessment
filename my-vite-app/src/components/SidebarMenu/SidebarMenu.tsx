eeb06b9c9a6490b6e4cf16d626cce240258266a8
R3ndrex <yacholohchtoli@gmail.com>
2025-11-19 22:23:49 +0200

    add component, type menuitem, add to sidebarmenu menu as prop

--- my-vite-app/src/components/SidebarMenu/SidebarMenu.tsx
+++ my-vite-app/src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,10 +1,43 @@
 import { useRef, useState, useEffect } from "react";
 import "./style.css";
+
+type MenuItem = {
+    label: string;
+    children?: MenuItem[];
+};
 type SidebarMenuProps = {
     state: "open" | "closed";
+    menu?: MenuItem[] | MenuItem;
 };
 
-export default function SidebarMenu({ state }: SidebarMenuProps) {
+function MenuItem({ item }: { item: MenuItem }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const hasChildren = item.children && item.children.length > 0;
+
+    return (
+        <li className={`sidebar-item`}>
+            <div
+                className="item-header"
+                onClick={() => hasChildren && setIsOpen(!isOpen)}
+                style={{ cursor: hasChildren ? "pointer" : "default" }}
+            >
+                {item.label}
+                {hasChildren && <span className="arrow"></span>}
+            </div>
+
+            {hasChildren && isOpen && (
+                <ul className={`sidebar-sublist`}>
+                    {item.children &&
+                        item.children.map((child: MenuItem, index: number) => (
+                            <MenuItem key={index} item={child} />
+                        ))}
+                </ul>
+            )}
+        </li>
+    );
+}
+
+export default function SidebarMenu({ state, menu = [] }: SidebarMenuProps) {
     const ref = useRef<HTMLElement | null>(null);
     const [visible, setVisible] = useState(state === "open");
     useEffect(() => {
@@ -25,11 +58,17 @@ export default function SidebarMenu({ state }: SidebarMenuProps) {
             >
                 ☰
             </button>
-            <aside ref={ref} className={`sidebar ${visible ? "" : "closed"}`}>
-                <ul className="sidebar-list">
-                    <li className="sidebar-item">View Map</li>
-                    <li className="sidebar-item">View Main Page</li>
-                </ul>
+            <aside
+                ref={ref}
+                className={`sidebar ${visible ? "open" : "closed"}`}
+            >
+                {menu.length > 0 && (
+                    <ul className="sidebar-list">
+                        {menu.map((item, index) => (
+                            <MenuItem key={index} item={item} />
+                        ))}
+                    </ul>
+                )}
             </aside>
         </>
     );
