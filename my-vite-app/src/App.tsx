53442c53d68dff7dee77a26413630344fb4f19bc
R3ndrex <yacholohchtoli@gmail.com>
2025-11-19 22:21:53 +0200

    Add type for menuitem, add menu.

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -1,6 +1,26 @@
 import Input from "./components/Input/Input";
 import SidebarMenu from "./components/SidebarMenu/SidebarMenu";
 import Toast from "./components/Toast/Toast";
+type MenuItem = {
+    label: string;
+    children?: MenuItem[];
+};
+
+const menu: MenuItem[] = [
+    { label: "Home" },
+    {
+        label: "Products",
+        children: [
+            {
+                label: "Phones",
+                children: [{ label: "Iphone" }, { label: "Samsung" }],
+            },
+            { label: "Laptops" },
+        ],
+    },
+    { label: "About" },
+];
+
 function App() {
     return (
         <>
@@ -9,7 +29,7 @@ function App() {
             <Toast type="info" text="This is an info toast" />
             <Toast type="success" text="This is a success toast" />
             <Toast type="error" text="This is an error toast" />
-            <SidebarMenu state="closed" />
+            <SidebarMenu state="closed" menu={menu} />
         </>
     );
 }
