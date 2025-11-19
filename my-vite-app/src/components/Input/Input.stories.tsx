06556039843eb434bb7c281fa59d60496b93294c
R3ndrex <yacholohchtoli@gmail.com>
2025-11-19 20:00:12 +0200

    Add input component and its assets

---
+++ my-vite-app/src/components/Input/Input.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import Input from './Input';
+
+const meta = {
+  component: Input,
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {}
+};
+
+export const TextNoClearable: Story = {
+  args: {
+    type: "text",
+    clearable: false
+  }
+};
+
+export const PasswordClearable: Story = {
+  args: {
+    clearable: true,
+    type: "password"
+  }
+};
+
+export const PasswordNotClearable: Story = {
+  args: {
+    type: "password",
+    clearable: false
+  }
+};
--- my-vite-app/src/components/Input/Input.tsx
+++ my-vite-app/src/components/Input/Input.tsx
@@ -1,3 +1,29 @@
-export function Input() {
-    return <input placeholder="This is an Input component" />;
+import { useState } from "react";
+import "./style.css";
+type InputProps = {
+    type: "password" | "text";
+    clearable: boolean;
+};
+
+export default function Input({
+    type = "password",
+    clearable = true,
+}: InputProps) {
+    const [text, setText] = useState("");
+    return (
+        <div className="input-div">
+            <input
+                type={type}
+                placeholder="Enter text"
+                value={text}
+                onChange={(e) => setText(e.currentTarget.value)}
+            />
+            {clearable && (
+                <button
+                    onClick={() => setText("")}
+                    className="clear-button"
+                ></button>
+            )}
+        </div>
+    );
 }
