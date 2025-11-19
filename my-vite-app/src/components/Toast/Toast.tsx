f1a64280aa662ec5623ca61a341473cf4b91874f
R3ndrex <yacholohchtoli@gmail.com>
2025-11-19 20:44:07 +0200

    add toast component with different types and timeout for showing class removing

--- my-vite-app/src/components/Toast/Toast.tsx
+++ my-vite-app/src/components/Toast/Toast.tsx
@@ -1,10 +1,32 @@
+import { useEffect } from "react";
+import "../../index.css";
 import "./style.css";
+
+function capitalize(str: string) {
+    if (str.length === 0) {
+        return "";
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export default function Toast({
     type,
-    children,
+    text,
 }: {
-    type?: "info" | "success" | "error";
-    children: React.ReactNode;
+    type: "info" | "success" | "error" | "warning";
+    text: string;
 }) {
-    return <div className={type}>{children}</div>;
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const toastElement = document.querySelector(`.toast.${type}`);
+            toastElement?.classList.remove("showing");
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, [type]);
+    return (
+        <div className={`toast showing ${type}`}>
+            <h2>{capitalize(type)}</h2>
+            {text}
+        </div>
+    );
 }
